d4c25826281567a0861d5804ef5872af471a8e82
rhtkdals135 <rhtkdals135@gmail.com>
2024-03-07 15:00:23 +0900

    Making log in page

---
+++ HTML/login.js
@@ -0,0 +1,14 @@
+function popId() {
+  const input = document.getElementById("txt_id").value;
+
+  if (!input)
+    alert("Please enter your ID");
+  else
+    alert(input);
+}
+
+function myFunction() {
+  alert("1");
+  alert("2");
+  alert("3");
+}
