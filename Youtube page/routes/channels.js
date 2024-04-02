a00ad2f224aaa38aa9c83e5e8e21674fef3d6649
dukkov <dukov.ko.dev@gmail.com>
2024-04-03 01:38:43 +0900

    validation

--- Youtube page/routes/channels.js
+++ Youtube page/routes/channels.js
@@ -1,8 +1,16 @@
 import express from "express";
+import conn from "../dbDemo.js";
+import {body, param, validationResult} from "express-validator";
 
 const router = express.Router();
-const channelDB = new Map();
-let key = 1;
+const validate = (req, res) => {
+  const err = validationResult(req);
+
+  if (!err.isEmpty())
+    return (res.status(400).json(err.array()));
+
+  next();
+};
 
 router.use(express.json());
 
@@ -10,76 +18,101 @@ router
   .route("/")
 
   // 전체 채널 조회
-  .get((req, res) => {
+  .get(body("userId").notEmpty().isInt().withMessage("Enter number"), (req, res) => {
     const {userId} = req.body;
-    const channels = [];
+    const err = validationResult(req);
+    const query = "SELECT * FROM channels WHERE user_id = ?";
 
-    if (channelDB.size && userId) {
-      channelDB.forEach((value) => {
-        if (value.userId === userId)
-          channels.push(value);
-      });
-      
-      res.status(200).json(channels);
-    } else {
-      res.status(404).json({ message: "Empty list" });
+    if (!err.isEmpty()) {
+      return (res.status(400).json(err.array()));
     }
+
+    conn.query(query, userId, (err, results) => {
+      if(results.length)
+        res.status(200).json(results);
+      else
+        res.status(404).json({ message: "Not found" });
+    });
   })
 
   // 채널 생성
-  .post((req, res) => {
-    if (req.body) {
-      const {channelTitle} = req.body;
+  .post([body("userId").notEmpty().isInt().withMessage("Enter number"), body("name").notEmpty().isString().withMessage("Enter string")], (req, res) => {
+    const err = validationResult(req);
 
-      channelDB.set(key++, req.body);
-      res.status(201).json({ message: `Channel ${channelTitle} created` });
-    } else {
-      res.status(400).json({ message: "Bad request" });
-    }
+    if (!err.isEmpty())
+      return (res.status(400).json({ message: "Bad request" }));
+
+    const {name, userId} = req.body;
+    const query = "INSERT INTO channels (name, user_id) VALUES (?, ?)";
+    const params = [name, userId];
+
+    conn.query(query, params, (err, results) => {
+      if (err)
+        return (res.status(500).end());
+
+      res.status(201).json(results);
+    });
   })
 
 router
   .route("/:id")
 
   // 개별 채널 조회
-  .get((req, res) => {
-    const id = parseInt(req.params.id);
-    const channel = channelDB.get(id);
+  .get(param("id").notEmpty().withMessage("Enter id"), (req, res) => {
+    const err = validationResult(req);
+    let {id} = req.params;
+    id = parseInt(id);
+    const query = "SELECT * FROM channels WHERE id = ?";
 
-    if (channel) {
-      res.status(200).json(channel);
-    } else {
-      res.status(404).json({ message: "Channel not found" });
-    }
+    if (!err.isEmpty())
+      return res.status(400).json(err.array());
+
+    conn.query(query, id, (err, results) => {
+      if (err)
+        return (res.status(500).end());
+      if(results.length)
+        res.status(200).json(results);
+      else
+      res.status(404).json({ message: "Not found" });
+    });
   })
 
   // 채널 수정
-  .put((req, res) => {
-    const id = parseInt(req.params.id);
-    const channel = channelDB.get(id);
+  .put([param("id").notEmpty().withMessage("Enter id"), body("name").notEmpty().isString().withMessage("Invalid title")], (req, res) => {
+    const err = validationResult(req);
+    let {id} = req.params;
+    id = parseInt(id);
+    const {name} = req.body;
+    const query = "UPDATE channels SET name = ? WHERE id = ?";
+    const params = [name, id];
+    
+    if (!err.isEmpty())
+      return res.status(400).json(err.array());
 
-    if (channel) {
-      const newTitle = req.body.channelTitle;
-      channel.channelTitle = newTitle;
-
-      channelDB.set(id, channel);
-      res.status(200).json({ message: `Channel title changed to ${newTitle}` });
-    } else {
-      res.status(404).json({ message: "Channel not found" });
-    }
+    conn.query(query, params, (err, results) => {
+      if (err)
+        return (res.status(500).end());
+      
+      return(200).json(results);
+    });
   })
 
   // 채널 삭제
-  .delete((req, res) => {
-    const id = parseInt(req.params.id);
-    const channel = channelDB.get(id);
+  .delete(param("id").notEmpty().withMessage("Enter title"),(req, res) => {
+    const err = validationResult(req);
+    let {id} = req.params;
+    id = parseInt(id);
+    const query = "DELETE FROM channels WHERE id = ?";
+    
+    if (!err.isEmpty())
+      return res.status(400).json(err.array());
 
-    if (channel) {
-      channelDB.delete(id);
-      res.status(200).json({ message: `${channel.channelTitle} delete done` });
-    } else {
-      res.status(404).json({ message: "Channel not found" });
-    }
+    conn.query(query, id, (err, results) => {
+      if (err)
+        return (res.status(500).end());
+      
+      return(200).json(results);
+    });
   })
 
 export default router;
