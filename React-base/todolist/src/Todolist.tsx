2fbcb6ccac3e9ef960169699032a936efca582de
dukkov <dukov.ko.dev@gmail.com>
2024-05-25 02:46:02 +0900

    Todolist checkbox updated

--- React-base/todolist/src/Todolist.tsx
+++ React-base/todolist/src/Todolist.tsx
@@ -1,17 +1,49 @@
 import React, { useState } from 'react';
 
+type Todo = {
+  id: number;
+  text: string;
+  isChecked: boolean;
+};
+
 const TodoList: React.FC = () => {
   const title: string = '오늘 할 일';
-  let [taskName, setTaskName] = useState<string>('일찍 자기');
+  let [tasks, setTasks] = useState<Todo[]>([
+    { id: 1, text: '일찍 자기', isChecked: false },
+    { id: 2, text: '공부하기', isChecked: false },
+    { id: 3, text: '책 읽기', isChecked: false }
+  ]);
+
+  const handleCheckedChange = (taskId: number) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, isChecked: !task.isChecked } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
-      <div className="board">
-        <h1>{title}</h1>
-        <ul>
-          <li>{taskName}</li>
-        </ul>
-      </div>
+      <h1>{title}</h1>
+      <div className="board" />
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <input
+              type="checkbox"
+              checked={task.isChecked}
+              onChange={() => handleCheckedChange(task.id)}
+            />
+            <span>
+              {task.isChecked ? (
+                <del>{task.text}</del>
+              ) : (
+                <span>{task.text}</span>
+              )}
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
